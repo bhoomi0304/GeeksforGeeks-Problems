3396e8176d57cff67e67bc599db6427267a1a709
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-06-03 15:51:28 +0530

    Create Permutation_In_Array.java

---
+++ Permutation_In_Array.java
@@ -0,0 +1,9 @@
+String str;
+        for(int i=0;i<s.length();i++){
+            
+            if(s.charAt(i)>=48 && s.charAt(i)<=57  || s.charAt(i)>=65 && s.charAt(i)<=90 || s.charAt(i)>=97 && s.charAt(i)<=122){
+                str[i]=s.charAt(i);
+            }
+            else
+                i--;
+    }
