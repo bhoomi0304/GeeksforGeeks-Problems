9308519cef2e1484dba084a332cdccb5b632e69a
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-06-17 23:04:25 +0530

    Update Permutation_In_Array.java

--- Permutation_In_Array.java
+++ Permutation_In_Array.java
@@ -2,7 +2,7 @@ String str;
         for(int i=0;i<s.length();i++){
             
             if(s.charAt(i)>=48 && s.charAt(i)<=57  || s.charAt(i)>=65 && s.charAt(i)<=90 || s.charAt(i)>=97 && s.charAt(i)<=122){
-                str[i]=s.charAt(i);
+                str[i]=s.charAt(i);//takes the perticular character from the string
             }
             else
                 i--;
