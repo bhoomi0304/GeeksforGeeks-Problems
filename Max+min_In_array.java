8e0a744357c89005f71fac39de693d98bd9e5674
Bhoomika <109805992+bhoomi0304@users.noreply.github.com>
2023-09-04 22:00:56 +0530

    Create Max+min_In_array.java

---
+++ Max+min_In_array.java
@@ -0,0 +1,17 @@
+class Solution
+{ 
+    public static int findSum(int A[],int N) 
+    {
+       int min=A[0];
+        int max=A[0];
+    	for(int i=1;i<N;i++){
+    	    if(A[i]>max){
+    	        max=A[i];
+    	    }
+    	    if(A[i]<min){
+    	        min=A[i];
+    	    }
+    	}
+    	return (max+min);  //code here
+    }
+}
